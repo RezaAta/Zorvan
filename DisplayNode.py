6d719e5e14ff9eb6a886d02f0d5c1b9733c410cb
RezaAta <35895202+RezaAta@users.noreply.github.com>
2024-10-21 20:57:52 +0330

    Add Display Node

---
+++ DisplayNode.py
@@ -0,0 +1,16 @@
+from BasicNode import BasicNode
+
+# BasicNode class (Abstract)
+class DisplayNode(BasicNode):  # Inherits from both Node and ABC
+    def __init__(self, name: str , value: int = 0):
+        super().__init__(name, value)  # Call Node's constructor
+        self.inputCount = 1  # Number of inputs
+        self.batchSize = 1  # Number of inputs
+        self.computationType = 'basic'  # Type of computation for the node
+
+    def Operation(self, input):
+        return input
+
+    def IsValidInput(self, inputs):
+        """Check if the inputs are valid. To be defined by subclasses."""
+        return True
