a3b07c85f578c3983944aa6209bea9c422992e2a
RezaAta <35895202+RezaAta@users.noreply.github.com>
2024-10-21 14:43:42 +0330

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -10,19 +10,18 @@ graph = Graph()
 
 node1 = AdditionNode(name="A", value=10)
 node2 = AdditionNode(name="B", value=20)
-node3 = MultiplicationNode(name="C", value=30)
+node3 = AdditionNode(name="C", value=30)
 
 graph.AddNode(node1,node2,node3)
-# graph.AddNode(node2)
-# graph.AddNode(node3)
-# graph.ConnectPreNode(node2, node1)  # A -> B
-# graph.ConnectPreNode(node3, node2)  # C -> B
-# graph.ConnectPreNode(node1, node3)  # B -> C
-# graph.ConnectPreNode(node1, node2)  # A -> B
-# graph.ConnectPreNode(node3, node1)  # A -> B
-# graph.ConnectPreNode(node2, node3)  # A -> B
+
+graph.ConnectPreNode(node2, node1)  # A -> B
+graph.ConnectPreNode(node3, node2)  # C -> B
+graph.ConnectPreNode(node1, node3)  # B -> C
+graph.ConnectPreNode(node1, node2)  # A -> B
+graph.ConnectPreNode(node3, node1)  # A -> B
+graph.ConnectPreNode(node2, node3)  # A -> B
 
 
 # Initialize processor with parallel execution
 processor = GraphProcessor(graph, max_workers=4)
-processor.ComputeGraph(iterations=5)
+processor.ComputeGraph(iterations=500)
