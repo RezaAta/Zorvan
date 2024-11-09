6b8f407c3d327a521c07fcc374075c21ce2fe9a0
RezaAta <35895202+RezaAta@users.noreply.github.com>
2024-11-09 14:21:21 +0330

    Add Mean Squared Error Node

---
+++ MeanSquaredErrorNode.py
@@ -0,0 +1,18 @@
+from BasicNode import BasicNode
+
+class MeanSquaredErrorNode(BasicNode):
+    def __init__(self, name: str = "", value=0):
+        super().__init__(name, value)
+        self.value = None
+        self.inclusive = False
+
+    def Operation(self, predicted, target):
+        squared_error = (predicted - target) ** 2
+        if self.value is None:
+            return squared_error
+        else:
+            return (squared_error + self.value) / 2
+
+    def IsValidInput(self, inp):
+        """Check if the input is a valid number (int or float)."""
+        return isinstance(inp, (int, float))
