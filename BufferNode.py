23d7058ca94a98a74952c26b50c9858962cda91a
RezaAta <35895202+RezaAta@users.noreply.github.com>
2024-11-18 09:37:06 +0330

    Fix bufferNode

--- BufferNode.py
+++ BufferNode.py
@@ -1,30 +1,25 @@
 from BasicNode import BasicNode
 
-class DataStreamNode(BasicNode):
-    def __init__(self, name: str = "", data = [], size = 1, initialDelay: int = 0, streamDelay: int = 0):
+class BufferNode(BasicNode):
+    def __init__(self, name: str = "", data=None, size: int = 1):
         super().__init__(name, 0)
-        self.buffer = data
-        if len(data) > size:
-            print("Buffer Overloaded! Extending Buffer.")
-            self.bufferSize = len(data)
-        else:
-            self.bufferSize = size
-
-        if data:  # Ensure data list is non-empty before accessing
-            self.value = data[0]
-        else:
-            self.value = None  # Default to None if data is empty
-
+        self.buffer = data if data else []  # Start with given data or an empty list
+        self.bufferSize = max(size, len(self.buffer))  # Set buffer size based on max of size or initial data length
+        self.value = self.buffer[0] if self.buffer else None
         self.batchSize = 1
         self.inclusive = False
 
     def Operation(self, input):
+        """Add new input to buffer and maintain size constraint."""
         self.buffer.append(input)
         if len(self.buffer) > self.bufferSize:
             self.buffer.pop(0)
+        return self.buffer[0]  # Set value to the oldest element
 
-        self.value = self.buffer[0]
+    def IsValidInput(self, input):
+        return True
 
-    def IsValidInput(self, inp):
-        pass
-
+    def ResetBuffer(self):
+        """Clear the buffer for fresh computations."""
+        self.buffer.clear()
+        self.value = None
