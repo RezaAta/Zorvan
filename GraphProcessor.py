93d0556b97646bd0e4c9f9df6e11ae4b8c651534
RezaAta <35895202+RezaAta@users.noreply.github.com>
2024-12-28 12:01:07 +0330

    Add Single Thread Computing

--- GraphProcessor.py
+++ GraphProcessor.py
@@ -23,7 +23,7 @@ class GraphProcessor:
         for t in range(iterations):
             if self.verbose:
                 print(f"\n--- Time Step {t+1} ---")
-
+        
             # 1. First loop: Update the inputs for all nodes in parallel
             with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
                 futures = [executor.submit(node.UpdateInputs) for node in self.graph.nodes if not node.midCalculation]
@@ -45,6 +45,37 @@ class GraphProcessor:
             self.time += 1
             if self.verbose:
                 print(f"End of time step {t+1}. Time is now {self.time}.")
-    
+    def ComputeGraphSingleThread(self, iterations=1):
+        """
+        Perform synchronous parallel computation for the graph over a number of iterations.
+        Each node first updates its inputs, then performs operations in two separate parallel loops.
+        """
+        if self.verbose:
+            print(f"Starting graph computation for {iterations} iterations...")
+
+        if self.verbose:
+            print("\n--- Time Step 0 ---")
+            for node in self.graph.nodes:
+                print(f"Node {node.name} has new value: {node.value}")
+
+        for t in range(iterations):
+            if self.verbose:
+                print(f"\n--- Time Step {t+1} ---")
+            for node in self.graph.nodes: 
+                if not node.midCalculation:
+                    node.UpdateInputs() 
+            for node in self.graph.nodes:
+                node.ProcessBatch() 
+
+            # Print node values after processing
+            if self.verbose:
+                for node in self.graph.nodes:
+                    print(f"Node {node.name} has new value: {node.value}")
+
+            # Increment time after completing the time step
+            self.time += 1
+            if self.verbose:
+                print(f"End of time step {t+1}. Time is now {self.time}.")
+
     def __repr__(self):
         return f"GraphProcessor with {len(self.graph.nodes)} nodes at time={self.time}."
