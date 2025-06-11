1b1dca3dd2d774f1c2d9f55501edc3c89be5f427
RezaAta <35895202+RezaAta@users.noreply.github.com>
2025-06-11 21:23:58 +0330

    Add the Fuzzy Extension to Computational Graphs
    Adds Nodes Max, Min, Gaussian, PL, and Division
    Adds a module to test the classic fuzzy control system for fanspeed
    Adds a module to test the computational graphs fuzzy extension for the fanspeed control problem

---
+++ ComputationalGraphs/Nodes/MinNode.py
@@ -0,0 +1,15 @@
+from ComputationalGraphs.Nodes.BasicNode import BasicNode
+
+class MinNode(BasicNode):
+    def __init__(self, name: str = ""):
+        super().__init__(name=name, value=0.0)
+        self.inputCount = 2
+        self.forcedBatchProcessing = True
+
+    def Operation(self, a:float, b:float ) -> float:
+        
+        return float(min([a,b]))
+
+    def IsValidInput(self, inp) -> bool:
+        return isinstance(inp, (int, float))
+
---
+++ ComputationalGraphs/Nodes/__init__.py
@@ -0,0 +1,46 @@
+from ComputationalGraphs.Nodes.AbstractNode import AbstractNode
+from ComputationalGraphs.Nodes.AdditionNode import AdditionNode
+from ComputationalGraphs.Nodes.CompressedNode import CompressedNode
+from ComputationalGraphs.Nodes.ContainerNode import ContainerNode
+from ComputationalGraphs.Nodes.DataStreamNode import DataStreamNode
+from ComputationalGraphs.Nodes.DisplayNode import DisplayNode
+from ComputationalGraphs.Nodes.DivisionNode import DivisionNode
+from ComputationalGraphs.Nodes.GaussianNode import GaussianNode
+from ComputationalGraphs.Nodes.LinearNode import LinearNode
+from ComputationalGraphs.Nodes.LinearNodeDerivative import LinearNodeDerivative
+from ComputationalGraphs.Nodes.MaxNode import MaxNode
+from ComputationalGraphs.Nodes.MinNode import MinNode
+from ComputationalGraphs.Nodes.MultiplicationNode import MultiplicationNode
+from ComputationalGraphs.Nodes.PiecewiseLinearNode import PiecewiseLinearNode
+from ComputationalGraphs.Nodes.MeanSquaredErrorNode import MeanSquaredErrorNode
+from ComputationalGraphs.Nodes.ReLUDerivativeNode import ReLUDerivativeNode
+from ComputationalGraphs.Nodes.ReLUNode import ReLUNode
+from ComputationalGraphs.Nodes.SigmoidDerivativeNode import SigmoidDerivativeNode
+from ComputationalGraphs.Nodes.SigmoidNode import SigmoidNode
+from ComputationalGraphs.Nodes.SubtractionNode import SubtractionNode
+
+
+__all__ = [
+    "AbstractNode",
+    "AdditionNode",
+    "CompressedNode",
+    "ContainerNode",
+    "DataStreamNode",
+    "DisplayNode",
+    "DivisionNode",
+    "GaussianNode",
+    "LinearNode",
+    "LinearNodeDerivative",
+    "MaxNode",
+    "MinNode",
+    "MultiplicationNode",
+    "PiecewiseLinearNode",
+    "MeanSquaredErrorNode",
+    "ReLUDerivativeNode",
+    "ReLUNode",
+    "SigmoidDerivativeNode",
+    "SigmoidNode",
+    "GaussianNode",
+    "SubtractionNode",
+    "MeanSquaredErrorNode"
+]
