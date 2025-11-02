1b8d617db1b6a02c190a475e9f75a27fd6f27fce
RezaAta <35895202+RezaAta@users.noreply.github.com>
2025-11-02 15:47:39 +0330

    Add ListNode and corresponding unit test
    Introduces a new ListNode class for accumulating inputs in a list, with an option to disallow None values and invalidate the batch if encountered. Also adds ListNodeTest.py to test ListNode's integration with other nodes in a computational graph.

---
+++ ComputationalGraphs/Nodes/ListNode.py
@@ -0,0 +1,37 @@
+from ComputationalGraphs.Nodes.BasicNode import BasicNode
+# ...existing code...
+class ListNode(BasicNode):
+    def __init__(self, name: str = "", allowNone: bool = False):
+        super().__init__(name, 0)
+        self.value = []
+        self.batchSize = 1
+        self.forcedBatchProcessing = True
+        self.inclusive = False
+        self.allowNone = allowNone
+        self._invalid_batch = False  # new flag to track invalidation within a timestep
+
+
+    def Operation(self, input):
+        # start fresh at the beginning of a batch
+        if self.midCalculation is False:
+            self.value = []
+            self._invalid_batch = False
+
+        # if a previous input already invalidated this batch, keep returning None
+        if self._invalid_batch:
+            return None
+
+        # if None values are not allowed, invalidate the whole output for this timestep
+        if not self.allowNone and input is None:
+            self.value = None
+            self._invalid_batch = True
+            return None
+
+        # otherwise append and return the accumulating list
+        if self.value is None:
+            self.value = []
+        self.value.append(input)
+        return self.value
+        
+    def IsValidInput(self, input):
+        return True
