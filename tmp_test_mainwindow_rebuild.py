02d7eb294239ea09540956340f5b131faad5c217
RezaAta <35895202+RezaAta@users.noreply.github.com>
2025-11-29 21:53:11 +0330

    Create tmp_test_mainwindow_rebuild.py

---
+++ tmp_test_mainwindow_rebuild.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from ComputationalGraphs.GUI.main_window import MainWindow
+from ComputationalGraphs.Nodes.DataStreamNode import DataStreamNode
+from ComputationalGraphs.Nodes.BufferNode import BufferNode
+
+app = QApplication(sys.argv)
+win = MainWindow()
+
+# Add nodes visually
+x = DataStreamNode(name='x', data=[1,2,3])
+b = BufferNode(name='b', size=3)
+
+win.canvas.add_node_item(x, 0, 0)
+win.canvas.add_node_item(b, 200, 0)
+# Rebuild underlying graph
+win.rebuild_graph()
+
+# Connect and then remove
+edge = win.canvas.add_edge_item(x, b)
+print('Before remove:', x in b.predecessors)
+edge.setSelected(True)
+win.canvas.remove_selected_items()
+print('After remove:', x in b.predecessors)
+
+app.quit()
