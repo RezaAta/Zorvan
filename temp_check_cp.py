2e9bf360130d0215dc6fc140c9268a7a1e4ebd04
RezaAta <35895202+RezaAta@users.noreply.github.com>
2026-05-27 01:17:52 +0330

    Remove temporary debug artifacts and add ignore rules

--- temp_check_cp.py
+++
@@ -1,9 +0,0 @@
-import importlib
-try:
-    import gui_framework.legacy.color_preferences as cp
-    print('cp module loaded', cp)
-    print('ColorPreferencesDialog', cp.ColorPreferencesDialog)
-except Exception as e:
-    import traceback
-    print('import failed', repr(e))
-    traceback.print_exc()
--- temp_check_legacy.py
+++
@@ -1,4 +0,0 @@
-from gui_framework.legacy import ColorPreferencesDialog, NodeEditorDialog, ReplaceNodeDialog
-print('ColorPreferencesDialog', ColorPreferencesDialog)
-print('NodeEditorDialog', NodeEditorDialog)
-print('ReplaceNodeDialog', ReplaceNodeDialog)
